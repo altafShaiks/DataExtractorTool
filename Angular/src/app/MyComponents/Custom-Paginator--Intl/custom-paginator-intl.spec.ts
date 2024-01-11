0fb7d8917c7045932c7bd0a3a9e14eb2b3b0f68e
Altaf <altaf.shaik@planonsoftware.com>
2024-01-11 13:19:47 +0530

    Wrote Users per page instead of Items per page at paginator

---
+++ Angular/src/app/MyComponents/Custom-Paginator--Intl/custom-paginator-intl.spec.ts
@@ -0,0 +1,7 @@
+import { CustomPaginatorIntl } from './custom-paginator-intl';
+
+describe('CustomPaginatorIntl', () => {
+  it('should create an instance', () => {
+    expect(new CustomPaginatorIntl()).toBeTruthy();
+  });
+});
